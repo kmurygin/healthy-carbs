43c0b930718986507a70f70c1525341cb6a9703c
Kacper Murygin <kacper.mur123@gmail.com>
2024-08-17 09:01:16 +0200

    feat: initialized spring boot project

---
+++ healthy-carbs/src/main/java/org/kmurygin/healthycarbs/HealthyCarbsApplication.java
@@ -0,0 +1,13 @@
+package org.kmurygin.healthycarbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyCarbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthyCarbsApplication.class, args);
+    }
+
+}
---
+++ healthy-carbs/src/test/java/org/kmurygin/healthycarbs/HealthyCarbsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kmurygin.healthycarbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyCarbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
