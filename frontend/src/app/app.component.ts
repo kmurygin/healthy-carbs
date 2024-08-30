421964029510c8b21022c2eb01676cda5642d488
Kacper Murygin <kacper.mur123@gmail.com>
2024-08-30 14:23:56 +0200

    implemented user model (later to be refactored), service and displaying users from db

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from "./user/user.component";
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:
+    [
+      RouterOutlet,
+      UserComponent,
+      NgFor
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
