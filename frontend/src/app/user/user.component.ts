b04b9d2a89abf219c40bcded02b0ba2cd59fea5a
Kacper Murygin <kacper.mur123@gmail.com>
2024-08-31 11:41:39 +0200

    refactor: added space

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import {Component, inject} from '@angular/core';
-import {UserService} from "../user.service";
-import {User} from "../models/user.model";
-import {NgFor, NgIf} from "@angular/common";
+import { Component, inject } from '@angular/core';
+import { UserService } from "../user.service";
+import { User } from "../models/user.model";
+import { NgFor, NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-user',
@@ -14,8 +14,8 @@ export class UserComponent {
   userService: UserService = inject(UserService);
   allUsers: User[] = [];
   constructor() {
-    this.userService.getAllUsers().then((housingLocationList: User[]) => {
-      this.allUsers = housingLocationList;
+    this.userService.getAllUsers().then((allUsers: User[]) => {
+      this.allUsers = allUsers;
     });
   }
 }
