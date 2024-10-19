d41512e4499131d1e118d11d0e291ae57f671ddc
Kacper Murygin <kacper.mur123@gmail.com>
2024-10-19 14:28:35 +0200

    refactor: reloading the page after successful login

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -40,6 +40,7 @@ export class LoginComponent {
         next: (response) => {
           console.log(response);
           this.router.navigate(['']);
+          window.location.reload();
         },
       });
     }
