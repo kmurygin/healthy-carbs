4037969a6241e839b4d7ac3ae8eef55a59f83f5b
Kacper Murygin <kacper.mur123@gmail.com>
2025-06-21 11:39:50 +0200

    feat: implement IndexComponent; update auth guard navigation

---
+++ frontend/src/app/pages/index/index.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-index',
+  imports: [
+    MatCardModule,
+  ],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.css'
+})
+export class IndexComponent {
+
+}
