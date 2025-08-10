607b272b404d2a87003504caca23457efedd6afe
Kacper Murygin <kacper.mur123@gmail.com>
2025-08-10 18:31:42 +0200

    refactor(guards): update `guestGuard` to use `canMatch` instead of `canActivate` and improve URL tree handling

--- frontend/src/app/core/guards/guest.guard.ts
+++ frontend/src/app/core/guards/guest.guard.ts
@@ -1,14 +1,14 @@
-import {CanActivateFn, Router} from '@angular/router';
+import {CanMatchFn, Router, UrlTree} from '@angular/router';
 import {inject} from "@angular/core";
 import {AuthService} from "../services/auth.service";
 
-export const guestGuard: CanActivateFn = (route, state) => {
+export const guestGuard: CanMatchFn = (route, segments): boolean | UrlTree => {
 
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (authService.isLoggedIn()) {
-    router.navigate(['']);
+    return router.parseUrl('');
   }
 
   return true;
