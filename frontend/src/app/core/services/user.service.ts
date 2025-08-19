a0410dcdbc4dd994d91e5a71ed1be154b80e7ef8
Kacper Murygin <kacper.mur123@gmail.com>
2025-08-19 18:26:07 +0200

    refactor(auth, user): update services

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -1,8 +1,9 @@
 import {inject, Injectable} from '@angular/core';
-import {ApiResponse, ChangePasswordPayload} from "../models/payloads";
-import {User} from "../models/user.model";
+import type {ChangePasswordPayload} from "../models/payloads";
+import type {UserDto} from "../models/dto/user.dto";
 import {ApiEndpoints} from "../constants/constants";
 import {HttpClient} from "@angular/common/http";
+import type {ApiResponse} from "../models/api-response.model";
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,14 @@ export class UserService {
   private httpClient = inject(HttpClient);
 
   getUserByUsername(username: string) {
-    return this.httpClient.get<ApiResponse<User>>(ApiEndpoints.User.GetUserByUsername + username);
+    return this.httpClient.get<ApiResponse<UserDto>>(ApiEndpoints.User.GetUserByUsername + username);
   }
 
-  updateUser(id: number, updatedUser: User) {
-    return this.httpClient.put<ApiResponse<User>>(ApiEndpoints.User.User + id, updatedUser);
+  updateUser(id: number, updatedUser: UserDto) {
+    return this.httpClient.put<ApiResponse<UserDto>>(ApiEndpoints.User.User + id, updatedUser);
   }
 
   changePassword(changePasswordPayload: ChangePasswordPayload) {
-    return this.httpClient.post<ApiResponse<any>>(ApiEndpoints.User.ChangePassword, changePasswordPayload);
+    return this.httpClient.post<ApiResponse<UserDto>>(ApiEndpoints.User.ChangePassword, changePasswordPayload);
   }
 }
