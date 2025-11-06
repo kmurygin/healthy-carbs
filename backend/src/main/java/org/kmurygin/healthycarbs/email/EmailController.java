33fae08b65bb76750c0c64e00d5775e0b3a9abf6
Kacper Murygin <kacper.mur123@gmail.com>
2025-11-06 11:40:50 +0100

    refactor(email): update EmailController mappings

--- backend/src/main/java/org/kmurygin/healthycarbs/email/EmailController.java
+++ backend/src/main/java/org/kmurygin/healthycarbs/email/EmailController.java
@@ -3,9 +3,11 @@ package org.kmurygin.healthycarbs.email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/mails")
 public class EmailController {
 
     private final EmailService emailService;
@@ -15,7 +17,7 @@ public class EmailController {
         this.emailService = emailService;
     }
 
-    @PostMapping("/sendMail")
+    @PostMapping
     public String
     sendMail(@RequestBody EmailDetails details) {
         emailService.sendMail(details);
