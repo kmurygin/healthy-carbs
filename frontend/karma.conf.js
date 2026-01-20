dbe0bd10140d6ff9ca9d5ab60bd38c6e1d58f798
Kacper Murygin <kacper.mur123@gmail.com>
2026-01-20 18:57:10 +0100

    chore(test): migrate Karma configuration to ES module format and update angular.json

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,38 +1,46 @@
-module.exports = function (config) {
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
-        plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage'),
-            require('@angular-devkit/build-angular/plugins/karma'),
-            require('karma-junit-reporter')
-        ],
-        client: {
-            jasmine: {
-            },
-            clearContext: false
-        },
-        jasmineHtmlReporter: {
-            suppressAll: true
-        },
-        coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/healthy-carbs'),
-            subdir: '.',
-            reporters: [
-                { type: 'html' },
-                { type: 'text-summary' }
-            ]
-        },
-        reporters: ['progress', 'kjhtml', 'junit'],
-        junitReporter: {
-            outputDir: 'test-results',
-            outputFile: 'test-results.xml',
-            useBrowserName: false
-        },
-        browsers: ['Chrome', 'ChromeHeadless'],
-        restartOnFileChange: true
-    });
+import {createRequire} from 'module';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+const require = createRequire(import.meta.url);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/healthy-carbs'),
+      subdir: '.',
+      reporters: [
+        {type: 'html'},
+        {type: 'text-summary'}
+      ]
+    },
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: require('path').join(__dirname, 'test-results'),
+      outputFile: 'test-results.xml',
+      useBrowserName: false
+    },
+    browsers: ['Chrome', 'ChromeHeadless'],
+    restartOnFileChange: true
+  });
 };
