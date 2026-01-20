5d245eb4222664c3242278e00e0a8a20a741bb82
Kacper Murygin <kacper.mur123@gmail.com>
2026-01-20 17:49:48 +0100

    chore(db): update enum changelog

--- backend/src/main/resources/db/sql/enums.sql
+++ backend/src/main/resources/db/sql/enums.sql
@@ -1,3 +1,16 @@
-CREATE TYPE gender AS ENUM ('MALE', 'FEMALE', 'OTHER');
-CREATE TYPE diet_type AS ENUM ('KETO', 'VEGAN', 'PALEO', 'BALANCED', 'STANDARD', 'VEGETARIAN', 'MEDITERRANEAN');
-CREATE TYPE activity_level AS ENUM ('SEDENTARY', 'LIGHT', 'MODERATE', 'HIGH', 'EXTREME');
+DO
+$$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'gender') THEN
+            CREATE TYPE gender AS ENUM ('MALE', 'FEMALE', 'OTHER');
+        END IF;
+
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'diet_type') THEN
+            CREATE TYPE diet_type AS ENUM ('KETO', 'VEGAN', 'PALEO', 'BALANCED', 'STANDARD', 'VEGETARIAN', 'MEDITERRANEAN');
+        END IF;
+
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'activity_level') THEN
+            CREATE TYPE activity_level AS ENUM ('SEDENTARY', 'LIGHT', 'MODERATE', 'HIGH', 'EXTREME');
+        END IF;
+    END
+$$;
