41d7b35628ecda2ff44111893ab642af662bc04d
Kacper Murygin <kacper.mur123@gmail.com>
2026-01-22 20:00:19 +0100

    chore(ci): add frontend lint job

--- frontend/src/app/features/dashboard/meal-plan-info/meal-plan-info.component.ts
+++ frontend/src/app/features/dashboard/meal-plan-info/meal-plan-info.component.ts
@@ -86,7 +86,7 @@ export class MealPlanInfoComponent {
 
       startWith<MealPlanState>(this.INITIAL_STATE),
 
-      catchError(() => of<MealPlanState>({
+      catchError((err: unknown) => of<MealPlanState>({
         status: 'error',
         error: ''
       }))
