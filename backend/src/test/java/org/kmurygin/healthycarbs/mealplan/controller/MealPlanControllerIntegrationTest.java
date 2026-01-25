087f6a96c8648a9d942649ecb30f94ebb4b8bc27
Kacper Murygin <kacper.mur123@gmail.com>
2026-01-25 17:35:38 +0100

    test(mealplan): activate 'test' profile in MealPlanControllerIntegrationTest

--- backend/src/test/java/org/kmurygin/healthycarbs/mealplan/controller/MealPlanControllerIntegrationTest.java
+++ backend/src/test/java/org/kmurygin/healthycarbs/mealplan/controller/MealPlanControllerIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(MealPlanController.class)
 @AutoConfigureMockMvc(addFilters = false)
 class MealPlanControllerIntegrationTest {
