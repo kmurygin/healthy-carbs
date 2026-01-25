13f5fa026e4e78fd5eff398bb5a5472a23b11f31
Kacper Murygin <kacper.mur123@gmail.com>
2026-01-25 17:32:40 +0100

    test(storage): mock StorageProvider in tests

--- backend/src/test/java/org/kmurygin/healthycarbs/HealthyCarbsApplicationTests.java
+++ backend/src/test/java/org/kmurygin/healthycarbs/HealthyCarbsApplicationTests.java
@@ -1,12 +1,17 @@
 package org.kmurygin.healthycarbs;
 
 import org.junit.jupiter.api.Test;
+import org.kmurygin.healthycarbs.storage.StorageProvider;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class HealthyCarbsApplicationTests {
+
+    @MockitoBean
+    private StorageProvider storageProvider;
 
     @Test
     void contextLoads() {
